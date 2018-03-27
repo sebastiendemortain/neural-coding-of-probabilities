4dd4cf60b79bf243c7c14476b7cbbb2c6b6c2aef
Sebastien Demortain <sebastien.demortain@gmail.com>
2018-03-27 14:44:19 +0200

    last update before the bold simulation

--- main_simu.py
+++ main_simu.py
@@ -6,92 +6,52 @@ from scipy import stats
 import numpy as np
 import matplotlib.pyplot as plt
 
+import import_distrib
 import distrib
 import tuning_curve
 import voxel
 
 # Define the seed to reproduce results from random processes
-rand.seed(1);
+rand.seed(2);
 
 # Import Matlab structure containing data and defining inputs
 data_mat = sio.loadmat('data/ideal_observer_toy_example.mat', struct_as_record=False)
-out = data_mat['out']
-out_HMM = out[0,0].HMM
 
-# The full distribution P(1|2) (resolution of 50 values and 600 trials)
-p1g2_dist = out_HMM[0,0].p1g2_dist
+[p1g2_dist, p1g2_mean, p1g2_sd] = import_distrib.import_distrib_param(data_mat)
 
-n_val = np.size(p1g2_dist, 0)
-n_trial = np.size(p1g2_dist, 1)
-
-# The MAP of this distribution
-p1g2_map = np.zeros(n_trial)
-x = np.linspace(0, 1, n_val)
-
-for k in range(0, n_trial):
-    map_idx = np.argmax(p1g2_dist[:, k])
-    p1g2_map[k] = x[map_idx]
-
-# The standard deviation of this distribution
-p1g2_sd = out_HMM[0, 0].p1g2_sd
-p1g2_sd = p1g2_sd[0, :]
-
-# PLOTTER STANDARD DEVIATION ! ! Pour tester
-
-# ## Test to check the loading and MAP/std calculations have been correctly performed
-#
-# # Select a random trial
-# trial = rand.randrange(n_trial)
-# y = p1g2_dist[:, trial]
-#
-# #Find the closest point from the linear spacing at the MAP
-# k = 0
-# while (x[k]<p1g2_map[trial]):
-#     k+=1
-#
-# plt.plot(x, y)    # Full distribution
-# plt.plot([p1g2_map[trial]], [y[k]], marker='o', markersize=3, color="red")    # Single point at the MAP
-# plt.text(p1g2_map[trial], y[k], 'MAP', fontsize=12, color='red')
-# plt.show()
-
-### Simulate signal from the data without noise
-
-# # Data of interest
-# q_map = p1g2_map
-# sigma_q = p1g2_sd   # We will consider the std only
+### Generate equivalent beta distribution
 
 # Generate data from beta distributions samples from means and std
-n_mean = 5    # Number of generated means
-n_sigma = 5    # Number of generated standard deviations
-q_mean = np.linspace(0.1, 0.9, n_mean)
-sigma_q = np.linspace(np.mean(p1g2_sd)-np.std(p1g2_sd), np.mean(p1g2_sd)+np.std(p1g2_sd), n_sigma)
+n_mean = 30    # Number of generated means
+n_sigma = 30    # Number of generated standard deviations
+q_mean = np.linspace(0.15, 0.85, n_mean)
+sigma = np.linspace(np.min(p1g2_sd), np.mean(p1g2_sd), n_sigma)
 
 # Creation of a list of simulated distributions
 simulated_distrib = [[None for j in range(n_sigma)] for i in range(n_mean)]
 
 for k_mean in range(n_mean):
     for k_sigma in range(n_sigma):
-        simulated_distrib[k_mean][k_sigma] = distrib.distrib(q_mean[k_mean], sigma_q[k_sigma])
+        simulated_distrib[k_mean][k_sigma] = distrib.distrib(q_mean[k_mean], sigma[k_sigma])
 
 # # Plots the distribution
 # fig = plt.figure()
-# k_mean = 3
-# k_sigma = 4
+# k_mean = -1
+# k_sigma = -1
 # y = simulated_distrib[k_mean][k_sigma].beta(x)
 # plt.plot(x, y)    # Full distribution
 # plt.show()
 
-
 # We find the variance of the data in order to scale equally activity from mean and activity from uncertainty
 q_mean_sd = np.std(q_mean)    # Variance of the signal of q_mean's
-sigma_q_sd = np.std(sigma_q)    # Variance of the signal of sigma_q's
+sigma_sd = np.std(sigma)    # Variance of the signal of sigma's
 
 # Lower and upper bounds of the encoded summary quantity (for tuning curves)
 tc_lower_bound_mean = 0
 tc_upper_bound_mean = 1
 tc_lower_bound_sigma = 0
 # we define the upper bound to be a bit away from the highest uncertainty
-tc_upper_bound_sigma = np.max(sigma_q)+2*sigma_q_sd
+tc_upper_bound_sigma = np.max(sigma)+2*sigma_sd
 
 # Resolution of the continuous plots
 plot_resolution = n_mean    # we consider the same resolution as for DPC
@@ -99,7 +59,7 @@ plot_resolution = n_mean    # we consider the same resolution as for DPC
 # we define the x-axis for varying mean
 x_mean = np.linspace(np.min(q_mean), np.max(q_mean), plot_resolution)
 # we define the x-axis for varying uncertainty
-x_sigma = np.linspace(np.min(sigma_q), np.max(sigma_q), plot_resolution)
+x_sigma = np.linspace(np.min(sigma), np.max(sigma), plot_resolution)
 
 
 ### 1) Rate coding simulation
@@ -107,42 +67,36 @@ x_sigma = np.linspace(np.min(sigma_q), np.max(sigma_q), plot_resolution)
 
 # Properties of the "rate voxel" to be simulated
 coding_scheme = 'rate'
-n_population = 2    # one for the mean, one for the std
-n_subpopulation = 1    # by definition of rate coding
-
+population_fraction = [0.5, 0.5]    # one for the mean, one for the std
 
 # Creation of the "rate voxel"
-rate_voxel = voxel.voxel(coding_scheme, n_population, n_subpopulation)
+rate_voxel = voxel.voxel(coding_scheme, population_fraction)
 
 # Computes the signal
-rate_signal = rate_voxel.activity(simulated_distrib, x_mean, x_sigma, q_mean_sd, sigma_q_sd)
+rate_signal = rate_voxel.activity(simulated_distrib, x_mean, x_sigma, q_mean_sd, sigma_sd)
 
 ### PPC simulation
 
-# TC related to the mean
+# Properties of the voxel to be simulated
+coding_scheme = 'ppc'
+population_fraction = [0.5, 0.5]    # Population fraction (one mean, one std)
 
+# TC related to the mean
 tc_type_mean = 'gaussian'    # Tuning curve type
 N_mean = 10    # Number of tuning curves
-t_mean = 0.08    # The best value from the previous "sum" analysis
-
+t_mean = 0.05    # The best value from the previous "sum" analysis
 # Creates the tuning_curve object
 tc_mean = tuning_curve.tuning_curve(tc_type_mean, N_mean, t_mean, tc_lower_bound_mean, tc_upper_bound_mean)
 
 # TC related to the uncertainty
-
 tc_type_sigma = 'gaussian'    # Tuning curve type
 N_sigma = 10    # Number of tuning curves
-t_sigma = 0.02    # The best value from the previous "sum" analysis
-
+t_sigma = 0.01    # The best value from the previous "sum" analysis
 # Creates the tuning_curve object
 tc_sigma = tuning_curve.tuning_curve(tc_type_sigma, N_sigma, t_sigma, tc_lower_bound_sigma, tc_upper_bound_sigma)
 
-# Properties of the voxel to be simulated
-coding_scheme = 'ppc'
-n_population = 2    # one mean and one std
-n_subpopulation = tc_mean.N    # here the mean and the std's tuning curves shall have the same N
-
-ppc_voxel = voxel.voxel(coding_scheme, n_population, n_subpopulation, [tc_mean, tc_sigma])
+# Creation of the "ppc voxel"
+ppc_voxel = voxel.voxel(coding_scheme, population_fraction, [tc_mean, tc_sigma])
 
 # Computes the signal
 ppc_signal = ppc_voxel.activity(simulated_distrib, x_mean, x_sigma)
@@ -151,129 +105,185 @@ ppc_signal = ppc_voxel.activity(simulated_distrib, x_mean, x_sigma)
 
 # Properties of the voxel to be simulated
 coding_scheme = 'dpc'
-n_population = 1    # population of tuning curves to be used for projection
-n_subpopulation = tc_mean.N
+population_fraction = [1]    # One population : related to tuning curves of the mean
 
-dpc_voxel = voxel.voxel(coding_scheme, n_population, n_subpopulation, [tc_mean])    # we only input the tuning curve of the mean
+# Creation of the "dpc voxel"
+dpc_voxel = voxel.voxel(coding_scheme, population_fraction, [tc_mean])    # we only input the tuning curve of the mean
+
+# We impose the same neuron fraction for DPC and PPC, for comparison
+dpc_voxel.subpopulation_fraction = np.expand_dims(ppc_voxel.subpopulation_fraction[0, :], axis=0)
 
 # Computes the signal
-dpc_signal = dpc_voxel.activity(simulated_distrib, x_mean)
+dpc_signal = dpc_voxel.activity(simulated_distrib, x_mean, x_sigma)
 
 ### PLOTS
 
 # Plot the signal for different voxel types and different distributions
-k_jump = 1    # To skip some curves
+k_jump = 6    # To skip some curves
 
 fig = plt.figure()
-plt.subplot(321)
-for k_sigma in range(0,n_sigma,k_jump):
-    plt.plot(x_mean, rate_signal[:, k_sigma], label='sigma_q='+str(round(sigma_q[k_sigma],2)))
-plt.xlabel('Inferred probability')
-plt.ylabel('Signal intensity')
-plt.title('Rate coding signal')
-plt.legend()
 
-plt.subplot(322)
+# Big subplots for common axes
+
+ax_left = fig.add_subplot(121)
+# Turn off axis lines and ticks of the big subplot
+ax_left.spines['top'].set_color('none')
+ax_left.spines['bottom'].set_color('none')
+ax_left.spines['left'].set_color('none')
+ax_left.spines['right'].set_color('none')
+ax_left.tick_params(labelcolor='w', top='off', bottom='off', left='off', right='off')
+ax_left.set_xlabel('Inferred probability')
+
+ax_right = fig.add_subplot(122)
+# Turn off axis lines and ticks of the big subplot
+ax_right.spines['top'].set_color('none')
+ax_right.spines['bottom'].set_color('none')
+ax_right.spines['left'].set_color('none')
+ax_right.spines['right'].set_color('none')
+ax_right.tick_params(labelcolor='w', top='off', bottom='off', left='off', right='off')
+ax_right.set_xlabel('Inferred standard deviation')
+
+
+ax_rate_mean = fig.add_subplot(421)
+for k_sigma in range(0,n_sigma,k_jump):
+    ax_rate_mean.plot(x_mean, rate_signal[:, k_sigma], label='sigma='+str(round(sigma[k_sigma],2)))
+ax_rate_mean.set_ylabel('Signal intensity')
+ax_rate_mean.set_title('Rate coding signal')
+ax_rate_mean.legend()
+ax_rate_mean.get_xaxis().set_ticks([])
+#ax_rate_mean.tick_params(axis='x', which='both', top='off', bottom='off', labelbottom='off')
+
+ax_rate_sd = fig.add_subplot(422)
 for k_mean in range(0,n_mean,k_jump):
-    plt.plot(x_sigma, rate_signal[k_mean, :], label='q_mean='+str(round(q_mean[k_mean],2)))
-plt.xlabel('Inferred standard deviation')
-plt.ylabel('Signal intensity')
-plt.title('Rate coding signal')
-plt.legend()
+    ax_rate_sd.plot(x_sigma, rate_signal[k_mean, :], label='q_mean='+str(round(q_mean[k_mean],2)))
+ax_rate_sd.set_ylabel('Signal intensity')
+ax_rate_sd.set_title('Rate coding signal')
+ax_rate_sd.legend()
+ax_rate_sd.get_xaxis().set_ticks([])
 
-plt.subplot(323)
+
+ax_ppc_mean = fig.add_subplot(423)
 for k_sigma in range(0,n_sigma,k_jump):
-    plt.plot(x_mean, ppc_signal[:, k_sigma], label='sigma_q='+str(round(sigma_q[k_sigma],2)))
-plt.xlabel('Inferred probability')
-plt.ylabel('Signal intensity')
-plt.title('PPC signal')
-plt.legend()
+    ax_ppc_mean.plot(x_mean, ppc_signal[:, k_sigma], label='sigma='+str(round(sigma[k_sigma],2)))
+ax_ppc_mean.set_ylabel('Signal intensity')
+ax_ppc_mean.set_title('PPC signal')
+ax_ppc_mean.legend()
+ax_ppc_mean.get_xaxis().set_ticks([])
 
-plt.subplot(324)
+ax_ppc_sd = fig.add_subplot(424)
 for k_mean in range(0, n_mean, k_jump):
-    plt.plot(x_sigma, ppc_signal[k_mean, :], label='q_mean='+str(round(q_mean[k_mean],2)))
-plt.xlabel('Inferred standard deviation')
-plt.ylabel('Signal intensity')
-plt.title('PPC signal')
-plt.legend()
+    ax_ppc_sd.plot(x_sigma, ppc_signal[k_mean, :], label='q_mean='+str(round(q_mean[k_mean],2)))
+ax_ppc_sd.set_ylabel('Signal intensity')
+ax_ppc_sd.set_title('PPC signal')
+ax_ppc_sd.legend()
+ax_ppc_sd.get_xaxis().set_ticks([])
 
-plt.subplot(325)
+
+ax_dpc_mean = fig.add_subplot(425)
 for k_sigma in range(0,n_sigma,k_jump):
-    plt.plot(x_mean, dpc_signal[:, k_sigma], label='sigma_q='+str(round(sigma_q[k_sigma],2)))
-plt.xlabel('Inferred probability')
-plt.ylabel('Signal intensity')
-plt.title('DPC signal')
-plt.legend()
+    ax_dpc_mean.plot(x_mean, dpc_signal[:, k_sigma], label='sigma='+str(round(sigma[k_sigma],2)))
+ax_dpc_mean.set_ylabel('Signal intensity')
+ax_dpc_mean.set_title('DPC signal')
+ax_dpc_mean.legend()
+ax_dpc_mean.get_xaxis().set_ticks([])
 
-plt.subplot(326)
+
+ax_dpc_sd = fig.add_subplot(426)
 for k_mean in range(0,n_mean,k_jump):
-    plt.plot(x_sigma, dpc_signal[k_mean, :], label='q_mean='+str(round(q_mean[k_mean],2)))
-plt.xlabel('Inferred standard deviation')
-plt.ylabel('Signal intensity')
-plt.title('DPC signal')
-plt.legend()
+    ax_dpc_sd.plot(x_sigma, dpc_signal[k_mean, :], label='q_mean='+str(round(q_mean[k_mean],2)))
+ax_dpc_sd.set_ylabel('Signal intensity')
+ax_dpc_sd.set_title('DPC signal')
+ax_dpc_sd.legend()
+ax_dpc_sd.get_xaxis().set_ticks([])
+
+width = np.mean(q_mean)/(2*ppc_voxel.tuning_curve[1].N)
+
+ax_frac_mean = fig.add_subplot(427)
+x = np.linspace(0, 1, ppc_voxel.tuning_curve[0].N)
+ax_frac_mean.bar(x, ppc_voxel.subpopulation_fraction[0], width=width)
+ax_frac_mean.set_ylabel('Fraction')
+ax_frac_mean.set_title('Neural fraction')
+
+width = np.mean(sigma)/(2*ppc_voxel.tuning_curve[1].N)
+
+ax_frac_sd = fig.add_subplot(428)
+x = np.linspace(tc_lower_bound_sigma, tc_upper_bound_sigma, ppc_voxel.tuning_curve[1].N)
+ax_frac_sd.bar(x, ppc_voxel.subpopulation_fraction[1], width=width)
+ax_frac_sd.set_ylabel('Fraction')
+ax_frac_sd.set_title('Neural fraction')
+
+
+ax_rate_mean.set_xlim([tc_lower_bound_mean, tc_upper_bound_mean])
+ax_rate_sd.set_xlim([tc_lower_bound_sigma, tc_upper_bound_sigma])
+ax_ppc_mean.set_xlim([tc_lower_bound_mean, tc_upper_bound_mean])
+ax_ppc_sd.set_xlim([tc_lower_bound_sigma, tc_upper_bound_sigma])
+ax_dpc_mean.set_xlim([tc_lower_bound_mean, tc_upper_bound_mean])
+ax_dpc_sd.set_xlim([tc_lower_bound_sigma, tc_upper_bound_sigma])
+ax_frac_mean.set_xlim([tc_lower_bound_mean, tc_upper_bound_mean])
+ax_frac_sd.set_xlim([tc_lower_bound_sigma, tc_upper_bound_sigma])
+
 plt.show()
 
 
 #############
 
-# # Plot the optimal tuning curves
-#
-# fig = plt.figure()
-# x = np.linspace(tc_lower_bound_mean,tc_upper_bound_mean,1000)
-# plt.subplot(211)
-# for i in range(0, N_mean):
-#     plt.plot(x, tc_mean.f(x, i))
-#
-# plt.xlabel('Preferred probability')
-# plt.ylabel('Tuning curve value')
-# plt.title('Optimal tuning curves for encoding the mean (N='+str(tc_mean.N)+')')
-#
-# plt.subplot(212)
-# x = np.linspace(tc_lower_bound_sigma,tc_upper_bound_sigma,1000)
-# for i in range(0, N_sigma):
-#     plt.plot(x, tc_sigma.f(x, i))
-#
-# plt.xlabel('Preferred standard deviation')
-# plt.ylabel('Tuning curve value')
-# plt.title('Optimal tuning curves for encoding the uncertainty (N='+str(tc_sigma.N)+')')
-# plt.show()
-#
-#
-# # Find the optimal tuning curves' standard deviation for fixed N : we consider the lowest std such that the sum over the TC is constant
-#
-# tested_t = [0.01,0.03,0.05,0.08,0.1,0.2,0.5]    # The different std
-# x = np.linspace(tc_lower_bound_mean, tc_upper_bound_mean, 1000)
-# tc_sum = np.zeros([len(tested_t), 1000])    # Initialization of the different TC sum's
-#
-# for k_t in range(len(tested_t)):
-#     tc = tuning_curve.tuning_curve(tc_type_mean, N_mean, tested_t[k_t], tc_lower_bound_mean, tc_upper_bound_mean)
-#     for i in range(N_mean):
-#         tc_sum[k_t, :] += tc.f(x, i)
-#
-# fig = plt.figure()
-# plt.subplot(211)
-# for k_t in range(len(tested_t)):
-#     plt.plot(x, tc_sum[k_t, :], label='t='+str(tested_t[k_t]))
-#
-# plt.xlabel('Probability')
-# plt.ylabel('Sum over the tuning curves')
-# plt.legend()
-#
-# tested_t = [0.005,0.01,0.02,0.03,0.05,0.08]    # The different std
-# x = np.linspace(tc_lower_bound_sigma, tc_upper_bound_sigma, 1000)
-# tc_sum = np.zeros([len(tested_t), 1000])
-#
-# for k_t in range(len(tested_t)):
-#     tc = tuning_curve.tuning_curve(tc_type_sigma, N_sigma, tested_t[k_t], tc_lower_bound_sigma, tc_upper_bound_sigma)
-#     for i in range(N_sigma):
-#         tc_sum[k_t, :] += tc.f(x, i)
-#
-# plt.subplot(212)
-# for k_t in range(len(tested_t)):
-#     plt.plot(x, tc_sum[k_t, :], label='t='+str(tested_t[k_t]))
-#
-# plt.xlabel('Probability')
-# plt.ylabel('Sum over the tuning curves')
-# plt.legend()
-# plt.show()
+# Plot the optimal tuning curves
+
+fig = plt.figure()
+x = np.linspace(tc_lower_bound_mean,tc_upper_bound_mean,1000)
+plt.subplot(211)
+for i in range(0, N_mean):
+    plt.plot(x, tc_mean.f(x, i))
+
+plt.xlabel('Preferred probability')
+plt.ylabel('Tuning curve value')
+plt.title('Optimal tuning curves for encoding the mean (N='+str(tc_mean.N)+')')
+
+plt.subplot(212)
+x = np.linspace(tc_lower_bound_sigma,tc_upper_bound_sigma,1000)
+for i in range(0, N_sigma):
+    plt.plot(x, tc_sigma.f(x, i))
+
+plt.xlabel('Preferred standard deviation')
+plt.ylabel('Tuning curve value')
+plt.title('Optimal tuning curves for encoding the uncertainty (N='+str(tc_sigma.N)+')')
+plt.show()
+
+
+# Find the optimal tuning curves' standard deviation for fixed N : we consider the lowest std such that the sum over the TC is constant
+
+tested_t = [0.01,0.03,0.05,0.08,0.1,0.2,0.5]    # The different std
+x = np.linspace(tc_lower_bound_mean, tc_upper_bound_mean, 1000)
+tc_sum = np.zeros([len(tested_t), 1000])    # Initialization of the different TC sum's
+
+for k_t in range(len(tested_t)):
+    tc = tuning_curve.tuning_curve(tc_type_mean, N_mean, tested_t[k_t], tc_lower_bound_mean, tc_upper_bound_mean)
+    for i in range(N_mean):
+        tc_sum[k_t, :] += tc.f(x, i)
+
+fig = plt.figure()
+plt.subplot(211)
+for k_t in range(len(tested_t)):
+    plt.plot(x, tc_sum[k_t, :], label='t='+str(tested_t[k_t]))
+
+plt.xlabel('Probability')
+plt.ylabel('Sum over the tuning curves')
+plt.legend()
+
+tested_t = [0.005,0.01,0.02,0.03,0.05,0.08]    # The different std
+x = np.linspace(tc_lower_bound_sigma, tc_upper_bound_sigma, 1000)
+tc_sum = np.zeros([len(tested_t), 1000])
+
+for k_t in range(len(tested_t)):
+    tc = tuning_curve.tuning_curve(tc_type_sigma, N_sigma, tested_t[k_t], tc_lower_bound_sigma, tc_upper_bound_sigma)
+    for i in range(N_sigma):
+        tc_sum[k_t, :] += tc.f(x, i)
+
+plt.subplot(212)
+for k_t in range(len(tested_t)):
+    plt.plot(x, tc_sum[k_t, :], label='t='+str(tested_t[k_t]))
+
+plt.xlabel('Probability')
+plt.ylabel('Sum over the tuning curves')
+plt.legend()
+plt.show()
--- voxel.py
+++ voxel.py
@@ -13,33 +13,41 @@ class voxel:
     alpha = 1
 
     # Initialization of the attributes of the voxel object
-    def __init__(self, coding_scheme, n_population, n_subpopulation, tc = [], sparsity_exp = 1):
+    def __init__(self, coding_scheme, population_fraction, tc = [], sparsity_exp = 1):
         self.coding_scheme = coding_scheme   # the type of coding scheme
-        self.n_population = n_population    # number of independent neural populations
+        self.population_fraction = population_fraction
+        self.n_population = len(self.population_fraction)    # number of independent neural populations
         self.sparsity_exp = sparsity_exp    # Exponent controlling the sparsity of the neuron fraction
-        # Fraction of each neural population
-        neuron_fraction = np.zeros([n_population, n_subpopulation])
-        n_neuron = np.zeros([n_population, n_subpopulation])    # Number of neuron per subpopulation
-        n_total_neuron = np.zeros(n_population)
-        for pop in range(n_population):
-            for subpop in range(n_subpopulation):
-                # Number of neuron per subpopulation controlled by a sparsity exponent
-                n_neuron[pop, subpop] = (rand.randrange(n_subpopulation)+1)**sparsity_exp
-            n_total_neuron[pop] = np.sum(n_neuron)
-        for pop in range(n_population):
-            for subpop in range(n_subpopulation):
-                # Fraction of neuron for each population. It sums up to 1 over each subpopulation
-                neuron_fraction[pop, subpop] = n_neuron[pop, subpop]/n_total_neuron[pop]
-
-        self.neuron_fraction = neuron_fraction
 
         self.tuning_curve = tc
         # If Probabilistic population code or distributional population code, we define one tuning curves' list
         if self.coding_scheme == 'ppc':
-            self.tuning_curve[0] = tc[0]
-            self.tuning_curve[1] = tc[1]
+            for pop in range(self.n_population):
+                self.tuning_curve[pop] = tc[pop]
+            # Number of neurons within one population (shall be the same for all populations of PPC) : number of TC
+            self.n_subpopulation = self.tuning_curve[0].N
         elif self.coding_scheme == 'dpc':
-            self.tuning_curve[0] = tc[0]
+            for pop in range(self.n_population):
+                self.tuning_curve[pop] = tc[pop]
+            self.n_subpopulation = self.tuning_curve[0].N    # this is the number of tuning curve
+        elif self.coding_scheme == 'rate':
+            self.n_subpopulation = 1    # By definition of rate coding
+
+        # Fraction of each neural population
+        subpopulation_fraction = np.zeros([self.n_population, self.n_subpopulation])
+        n_neuron = np.zeros([self.n_population, self.n_subpopulation])    # Number of neuron per subpopulation
+        n_total_neuron = np.zeros(self.n_population)
+        for pop in range(self.n_population):
+            for subpop in range(self.n_subpopulation):
+                # Number of neuron per subpopulation controlled by a sparsity exponent
+                n_neuron[pop, subpop] = (rand.randrange(self.n_subpopulation)+1)**sparsity_exp
+            n_total_neuron[pop] = np.sum(n_neuron[pop, :])
+        for pop in range(self.n_population):
+            for subpop in range(self.n_subpopulation):
+                # Fraction of neuron for each population. It sums up to 1 over each subpopulation
+                subpopulation_fraction[pop, subpop] = n_neuron[pop, subpop]/n_total_neuron[pop]
+
+        self.subpopulation_fraction = subpopulation_fraction
 
     # Neural activity given one distribution
     def activity(self, distrib_array, x_mean, x_sigma=[np.nan], sigma_mean=np.nan, sigma_sd=np.nan):
@@ -59,27 +67,30 @@ class voxel:
                     for k_sigma in range(len(distrib_array[0])):
                         distrib = distrib_array[k_mean][k_sigma]
                         q_mean = distrib.mean  # Mean of the distribution
-                        sigma_q = distrib.sd  # Standard deviation of the distribution
-                        activity[k_mean, k_sigma] = self.neuron_fraction[0, 0]*q_mean \
-                                                    + (sigma_mean/sigma_sd)*self.neuron_fraction[1, 0]*sigma_q
+                        sigma = distrib.sigma  # Standard deviation of the distribution
+                        activity[k_mean, k_sigma] = self.population_fraction[0]*self.subpopulation_fraction[0, 0]\
+                                                    *q_mean + (sigma_mean/sigma_sd)*self.population_fraction[1]\
+                                                              *self.subpopulation_fraction[1, 0]*sigma
 
         elif self.coding_scheme == 'ppc':    # Probabilistic population coding case
             # Activity due to mean encoding
             for i in range(self.tuning_curve[0].N):
-                frac_mean = self.neuron_fraction[0, i]
-                frac_sigma = self.neuron_fraction[1, i]
+                frac_mean = self.subpopulation_fraction[0, i]
+                frac_sigma = self.subpopulation_fraction[1, i]
                 f_mean = self.tuning_curve[0].f(x_mean, i)
                 f_sigma = self.tuning_curve[1].f(x_sigma, i)
                 for k_mean in range(n_mean):
                     for k_sigma in range(n_sigma):
-                        activity[k_mean, k_sigma] += frac_mean*f_mean[k_mean]+frac_sigma*f_sigma[k_sigma]
+                        activity[k_mean, k_sigma] += self.population_fraction[0]*frac_mean*f_mean[k_mean]\
+                                                     + self.population_fraction[1]*frac_sigma*f_sigma[k_sigma]
 
         elif self.coding_scheme == 'dpc':    # Distributional population coding case
-            res = 10000    # Number of points used for the numerical integral
-            delta_x = (self.tuning_curve[0].upper_bound-self.tuning_curve[0].lower_bound)/(res-1)    # Integral step
+            res = 1e5    # Number of points used for the numerical integral
+            eps = 0  # Reduction of the interval for dealing with boundary issues of the beta function
             # x-axis for integration
-            x = np.linspace(self.tuning_curve[0].lower_bound, self.tuning_curve[0].upper_bound, res)
-            x = np.delete(x, -1)    # res-1 points shall be considered for the numerical integration
+            x = np.linspace(self.tuning_curve[0].lower_bound+eps, self.tuning_curve[0].upper_bound-eps, res)
+            x = np.delete(x, 0)    # res-1 points shall be considered for the numerical integration
+            delta_x = (self.tuning_curve[0].upper_bound-self.tuning_curve[0].lower_bound-2*eps)/(res-2)    # Integral step
 
             # Double list containing the array of the beta function at the desired resolution
             beta = [[None for j in range(n_sigma)] for i in range(n_mean)]
@@ -88,16 +99,23 @@ class voxel:
                     distrib = distrib_array[k_mean][k_sigma]
                     beta[k_mean][k_sigma] = distrib.beta(x)
 
+
+            # sum_err = 0
             # Activity due to the projection on the tuning curves
             for i in range(self.tuning_curve[0].N):
                 # Projection of the distribution on tuning curve i
                 f = self.tuning_curve[0].f(x, i)    # tuning curve i's values along the x-axis
-                frac = self.neuron_fraction[0, i]
+                frac = self.subpopulation_fraction[0, i]
                 for k_mean in range(n_mean):
                     for k_sigma in range(n_sigma):
+                        # distrib = distrib_array[k_mean][k_sigma]
                         proj = np.dot(beta[k_mean][k_sigma], f)*delta_x
+                        # proj_num = integrate.quad(lambda y: distrib.beta(y)*self.tuning_curve[0].f(y, i), self.tuning_curve[0].lower_bound+eps, self.tuning_curve[0].upper_bound-eps)[0]
                         activity[k_mean, k_sigma] += frac*proj
-
+                        # sum_err += np.abs(proj-proj_num)/np.sqrt(np.dot(proj,proj))
+                        # if np.abs(proj-proj_num) > 0.01
+                        #     print('ISSUE WITH THE NUMERICAL INTEGRATION. See the dpc case in voxel.activity')
+            # print(sum_err)
         # Multiplication by the gain of the signal
         activity = self.alpha * activity
         return activity
